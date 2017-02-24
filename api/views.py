7530cbd6298d884b1ec7d4e6ab384c1aa978fe16
Ali Minaei <ali.minaei@uk-mb-040.wonga.com>
2017-02-24 04:06:55 +0000

    sender_id

--- api/views.py
+++ api/views.py
@@ -23,35 +23,16 @@ def setup_webhook(request):
         return HttpResponse("NOT FOUND", status=400)
     
 def parse_message(request):
-    data = json.loads(request.body)['entry'][0]['messaging'][0]["message"]["text"]
-    print data
+    
+    data = ""
+    sender_id = ""
+    try:
+        data = json.loads(request.body)['entry'][0]['messaging'][0]["message"]["text"]
+        sender_id = json.loads(request.body)['entry'][0]['messaging'][0]["sender"]["id"]
+    except:
+        data = ""
+        sender_id = ""
 
-
-        # {
-        #     "object":"page",
-        #     "entry":[
-        #         {
-        #             "messaging":[
-        #                 {
-        #                     "message":{
-        #                         "text":"does this work?",
-        #                         "seq":20,
-        #                         "mid":"mid.1466015596912:7348aba4de4cfddf91"
-        #                     },
-        #                     "timestamp":1466015596919,
-        #                     "sender":{
-        #                         "id":"885721401551027"
-        #                     },
-        #                     "recipient":{
-        #                         "id":"260317677677806"
-        #                     }
-        #                 }
-        #             ],
-        #             "time":1466015596947,
-        #             "id":"260317677677806"
-        #         }
-        #     ]
-        # }
-
+    print sender_id
 
     return HttpResponse(data)
